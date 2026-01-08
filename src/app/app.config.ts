64d5d53a211d683731dd256bfacc344c75fe617f
MEMOUE <memoue07@gmail.com>
2026-01-08 09:07:40 +0000

    fix: update component login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http'; // 1. Import this
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(), // 2. Add this here
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations() // Requis pour PrimeNG
+    provideAnimations()
   ]
 };
