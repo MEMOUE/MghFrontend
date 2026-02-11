b499f029dfa0bd84c2951a696f631370ebdf3e13
Memko <memko021@gmail.com>
2026-02-11 10:42:31 +0000

    feat: add component creatfinance

---
+++ src/app/components/finances/creatfinance/creatfinance.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creatfinance',
+  imports: [],
+  templateUrl: './creatfinance.html',
+  styleUrl: './creatfinance.css'
+})
+export class Creatfinance {
+
+}
