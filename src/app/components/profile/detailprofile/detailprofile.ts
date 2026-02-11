596c7313700494552267df0ea3409dd2e4ee678e
Memko <memko021@gmail.com>
2026-02-11 10:28:32 +0000

    feat: add the components profile

---
+++ src/app/components/profile/detailprofile/detailprofile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailprofile',
+  imports: [],
+  templateUrl: './detailprofile.html',
+  styleUrl: './detailprofile.css'
+})
+export class Detailprofile {
+
+}
---
+++ src/app/components/profile/edite-profile/edite-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edite-profile',
+  imports: [],
+  templateUrl: './edite-profile.html',
+  styleUrl: './edite-profile.css'
+})
+export class EditeProfile {
+
+}
